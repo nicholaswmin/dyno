e84ee218f90d05e40266020cbec4d03f685070d6
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-08-25 21:33:39 +0300

    test: tweaks

--- src/threadpool/test/example.test.js
+++ src/threadpool/test/example.test.js
@@ -1,16 +1,31 @@
 import test from 'node:test'
 import { join } from 'node:path'
 import { promisify } from 'node:util'
-import { execFile as execFileCB } from 'node:child_process'
+import { execFile } from 'node:child_process'
 
-test('example: pingpong', { timeout: 5000 }, async t => {
+test('example: pingpong', async t => {
   let out = null
 
   t.before(async () => {
-    out = await promisify(execFileCB)(
-      'node', [ '--no-warnings', '--run', 'pingpong' ], 
-      { cwd: join(import.meta.dirname, '../'), stdio: 'pipe', encoding: 'utf8' }
-    )
+    const ctrl = new AbortController()
+    setTimeout(() => ctrl.abort(), 1000)
+    
+    try {
+      out = await promisify(execFile)(
+        'node', [ '--no-warnings', '--run', 'pingpong' ], 
+        { 
+          cwd: join(import.meta.dirname, '../'), 
+          stdio: 'pipe', 
+          encoding: 'utf8',
+          signal: ctrl.signal
+        }
+      )
+    } catch (err) {
+      if (err.code !== 'ABORT_ERR')
+        throw err
+      
+      out = err
+    }
   })
   
   await t.test('logs in stdout', t => {
@@ -22,23 +37,15 @@ test('example: pingpong', { timeout: 5000 }, async t => {
     t.assert.ok(!out.stderr, `logged in stderr: ${out.stderr}`)
   })
   
-  await t.test('logs "ping"', async t => {
-    await t.test('logs "ping" word in stdout', t => {
-      t.assert.ok(out.stdout.includes('ping'), 'cannot find "ping" in stdout')
-    })
-    
-    await t.test('multiple "ping"s', t => {
-      t.assert.ok(out.stdout.split('ping').length > 5, '< 5 "ping" in stdout')
-    })
+  await t.test('logs at least 5 "ping"', async t => {
+    const found = out.stdout.split('ping').length
+
+    t.assert.ok(found > 3, `found: ${found} "ping" in stdout, must be >= 3`)
   })
   
-  await t.test('logs "pong"', async t => {
-    await t.test('logs "pong"', t => {
-      t.assert.ok(out.stdout.includes('pong'), 'cannot find "pong" in stdout')
-    })
-    
-    await t.test('multiple "pong"s', t => {
-      t.assert.ok(out.stdout.split('pong').length > 5, '< 5 "pong" in stdout')
-    })
+  await t.test('logs at least 5 "pong"', async t => {
+    const found = out.stdout.split('pong').length
+
+    t.assert.ok(found > 3, `found: ${found} "pong" in stdout, must be >= 3`)
   })
 })
