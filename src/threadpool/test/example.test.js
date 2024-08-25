2705f3a9ff946a1db5ad74feaa5bf81f7ac9f118
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-08-25 21:27:18 +0300

    test: tweaks

--- src/threadpool/test/example.test.js
+++ src/threadpool/test/example.test.js
@@ -7,11 +7,10 @@ test('example: pingpong', { timeout: 5000 }, async t => {
   let out = null
 
   t.before(async () => {
-    out = await promisify(execFileCB)('node', [
-      '--no-warnings', '--run', 'pingpong'
-    ], { 
-      cwd: join(import.meta.dirname, '../'), stdio: 'pipe', encoding: 'utf8' 
-    })
+    out = await promisify(execFileCB)(
+      'node', [ '--no-warnings', '--run', 'pingpong' ], 
+      { cwd: join(import.meta.dirname, '../'), stdio: 'pipe', encoding: 'utf8' }
+    )
   })
   
   await t.test('logs in stdout', t => {
