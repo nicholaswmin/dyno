ae636a3239d39a952009b9dd5e95567825ab17a5
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-08-26 00:16:54 +0300

    test: tweaks

--- src/threadpool/test/task/pong.js
+++ src/threadpool/test/task/pong.js
@@ -1,7 +1,4 @@
-// sends `pong` in response to a `ping`
+// emits `pong` in response to a `ping`
 import { primary } from '../../index.js'
 
-primary.on('ping', () => 
-  queueMicrotask(() => primary.emit('pong', { id: process.pid })))
-
-process.on('message', msg => msg === 'exit' ? process.exit(0) : null)
+primary.on('ping', () => setImmediate(() => primary.emit('pong')))
