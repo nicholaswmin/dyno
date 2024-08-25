3b254d48801aeec3b360ba47ebf59d3d70f421fa
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-08-25 18:56:48 +0300

    test: tweaks

--- src/threadpool/test/args.test.js
+++ src/threadpool/test/args.test.js
@@ -3,14 +3,13 @@ import { Threadpool } from '../index.js'
 
 test('#constructor()', async t => {
   await t.test('all arguments provided in valid format', async t => {
-    await t.test('does not throw', t => {
+    await t.test('instantiates ok', t => {
       t.assert.doesNotThrow(() => new Threadpool('task.js', 4, { foo: 'bar' }))
-      t.assert.ok(() => new Threadpool('task.js', 4, { foo: 'bar' }))
     })
   })
 
   await t.test('no arguments provided', async t => {
-    await t.test('instantiates', t => {
+    await t.test('instantiates ok', t => {
       t.assert.doesNotThrow(() => new Threadpool())
       t.assert.ok(() => new Threadpool())
     })
@@ -86,7 +85,7 @@ test('#constructor()', async t => {
     })
     
     await t.test('without any properties', async t => {
-      await t.test('is ok', t => {
+      await t.test('instantiates ok', t => {
         t.assert.doesNotThrow(() => new Threadpool(null, 3, {}))
       })
     })
