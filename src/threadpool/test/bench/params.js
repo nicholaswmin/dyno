72286d39d6789c8335d416e20296e9d103ef68b1
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-08-29 22:56:44 +0300

    test: ipc test

--- src/threadpool/test/bench/params.js
+++ src/threadpool/test/bench/params.js
@@ -3,10 +3,12 @@ import { availableParallelism } from 'node:os'
 import { parseArgs, styleText } from 'node:util'
 
 let path = join(import.meta.dirname, 'thread.js') 
-let { values: { type, size, kibs }  } = parseArgs({ options: { 
-  size: { type: 'string', default: availableParallelism().toString() },
-  kibs: { type: 'string', default: '1' }
-}})
+let { values: { type, size, kibs }  } = parseArgs({ 
+  options: { 
+    size: { type: 'string', default: availableParallelism().toString() },
+    kibs: { type: 'string', default: '1' }
+  }
+})
 
 size = +size, kibs = +kibs
 
