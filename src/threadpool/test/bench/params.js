a017650cf98301c0732363ae38b91c0dc29bc976
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-08-29 22:57:09 +0300

    test: ipc test

--- src/threadpool/test/bench/params.js
+++ src/threadpool/test/bench/params.js
@@ -1,6 +1,6 @@
 import { join } from 'node:path'
 import { availableParallelism } from 'node:os'
-import { parseArgs, styleText } from 'node:util'
+import { parseArgs } from 'node:util'
 
 let path = join(import.meta.dirname, 'thread.js') 
 let { values: { type, size, kibs }  } = parseArgs({ 
